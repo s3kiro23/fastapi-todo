e38cb5703ef9c011ae00e9a344f75b3db2f603c7
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-03-15 10:49:45 +0100

    add docker deploy

---
+++ deploy/prod/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source /opt/fastapi_todo/config.ini
+
+echo "AWS Region: $AWS_REGION"
+echo "AWS Account ID: $AWS_ACCOUNT_ID"
+
+# Replace these values with your specific ones
+APP_NAME="fastapi_todo"
+APP_PATH="/opt/dev_custom/projects/$APP_NAME/"
+
+cd $APP_PATH
+cd deploy/prod
+
+#git stash
+#git checkout main
+#git pull origin main
+
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$APP_NAME:latest
+
+LOG_DIR="/var/dev_data/$APP_NAME/log"
+DATA_DIR="/var/dev_data/$APP_NAME"
+
+if [ ! -d $LOG_DIR ]; then
+    mkdir -p $LOG_DIR
+    chown -R www-data:www-data $LOG_DIR
+fi
+
+if [ ! -d $DATA_DIR ]; then
+    mkdir -p $DATA_DIR
+    chown -R www-data:www-data $DATA_DIR
+fi
+
+docker-compose down -v
+docker-compose up -d
